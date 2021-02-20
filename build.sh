23415f082147042b1d70c353a26b9735cb2ead2a
DanielGavin <danielgavin5@hotmail.com>
2021-02-20 15:50:48 +0100

    switch to debug mode...

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -x
 
-odin build src/ -show-timings -microarch:native -collection:shared=src -out:ols -opt:1
+#debug mode is the only version that works on linux right now...
+odin build src/ -show-timings -microarch:native -collection:shared=src -debug -out:ols
+
+#odin build src/ -show-timings  -collection:shared=src -out:ols -opt:2
+
