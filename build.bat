f9ce5fd6cc6548bb4a1a332ea057a30be165b3d0
Daniel Gavin <danielgavin5@hotmail.com>
2021-04-23 13:43:26 +0200

    fix the build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-rem C:/Users/danie/OneDrive/Desktop/Computer_Science/Odin-fork/odin.exe build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -debug -llvm-api
+rem odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -debug -llvm-api
 
-C:/Users/danie/OneDrive/Desktop/Computer_Science/Odin-fork/odin.exe build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
+odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
