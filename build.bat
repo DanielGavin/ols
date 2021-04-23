946882d093d870f114b84872bc068f283339f762
Daniel Gavin <danielgavin5@hotmail.com>
2021-04-23 13:42:51 +0200

    move to the new llvm-api for opt builds

--- build.bat
+++ build.bat
@@ -1,16 +1,5 @@
 @echo off
 
+rem C:/Users/danie/OneDrive/Desktop/Computer_Science/Odin-fork/odin.exe build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -debug -llvm-api
 
-rem odin run tests\ -show-timings -llvm-api -collection:shared=src -microarch:native -out:test
-
-rem odin build tests\ -show-timings  -collection:shared=src -microarch:native -out:test -debug
-
-
-rem odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -debug
-
-
-rem odin build src\ -llvm-api -show-timings -microarch:native -collection:shared=src -out:ols -debug
-
-odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:3
-
-
+C:/Users/danie/OneDrive/Desktop/Computer_Science/Odin-fork/odin.exe build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
