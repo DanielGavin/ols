e0b425a5095f4d1f2066868ac88f418703b4e4e8
Daniel Gavin <danielgavin5@hotmail.com>
2021-04-25 19:09:22 +0200

    add testing to build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,19 @@
 @echo off
 
-rem odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -debug -llvm-api
 
 if "%1" == "CI" (
     set ODIN="Odin/odin"
 ) else (
-    set ODIN="odin"
+    set ODIN="C:\Users\danie\OneDrive\Desktop\Computer_Science\Odin-fork\odin"
 )
 
+%ODIN% test tests -llvm-api
+
+if %errorlevel% neq 0 goto end_of_build
+
 %ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
+rem %ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -llvm-api -debug
+
+
+
+:end_of_build
