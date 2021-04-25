2f5b07629beeee62e90cac417e67aea32462f737
DanielGavin <danielgavin5@hotmail.com>
2021-04-25 15:27:07 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,10 @@
 
 rem odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -debug -llvm-api
 
-odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
+if "%1" == "CI" (
+    set ODIN="../Odin/odin"
+) else (
+    set ODIN="odin"
+)
+
+%ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
