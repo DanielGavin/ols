de3a012bba5966adb31f69b190277e7083b2ab15
Daniel Gavin <danielgavin5@hotmail.com>
2021-04-27 00:12:57 +0200

    update build without llvm-api

--- build.bat
+++ build.bat
@@ -7,12 +7,12 @@ if "%1" == "CI" (
     set ODIN="odin"
 )
 
-%ODIN% test tests -llvm-api
+%ODIN% test tests -collection:shared=src
 
 if %errorlevel% neq 0 goto end_of_build
 
-%ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -llvm-api
-rem %ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0 -llvm-api -debug
+%ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 
+rem %ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0  -debug
 
 
 
