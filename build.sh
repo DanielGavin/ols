ea6ae1ab3333e42778e1cf4663dd62e2666ecbdf
DanielGavin <danielgavin5@hotmail.com>
2021-05-09 14:41:53 +0200

    fixed build.sh again..

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
 if [[ $1 == "CI" ]]
+then 
+    ODIN="Odin/odin"
+else
+    ODIN="odin"
+fi
+
+
+if [[ $1 == "CI" ]]
 then
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    ../Odin/odin test ../tests -collection:shared=../src
+    ../${ODIN} test ../tests -collection:shared=../src
 
     if ([ $? -ne 0 ])
     then
@@ -16,4 +24,4 @@ then
     cd ..
 fi
 
-odin build src/ -show-timings  -collection:shared=src -out:ols -opt:2
+${ODIN} build src/ -show-timings  -collection:shared=src -out:ols -opt:2
