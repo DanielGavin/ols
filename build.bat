37753dbe0b095a6ee30f5606ba45fe0d9aa84928
Daniel Gavin <danielgavin5@hotmail.com>
2021-08-19 15:56:32 +0200

    only run test in ci

--- build.bat
+++ build.bat
@@ -3,17 +3,10 @@
 
 if "%1" == "CI" (
     set ODIN="Odin/odin"
+    %ODIN% test tests -collection:shared=src -debug -opt:0 
+    if %errorlevel% neq 0 if "%x1" == "CI" exit 1
 ) else (
     set ODIN="odin"
 )
 
-%ODIN% test tests -collection:shared=src
-
-if %errorlevel% neq 0 goto end_of_build
-
-%ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 
-rem %ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:0  -debug
-
-
-
-:end_of_build
+%ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2
