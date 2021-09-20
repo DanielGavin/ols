d009425a92f4bf7e60ebcbf614532e23cdda914d
Daniel Gavin <danielgavin5@hotmail.com>
2021-09-21 00:34:55 +0200

    barch still not working for some strange reason.

--- build.bat
+++ build.bat
@@ -2,9 +2,9 @@
 
 
 if "%1" == "CI" (
-    Odin/odin test tests -collection:shared=src -debug -opt:0 
+    Odin/odin.exe test tests -collection:shared=src -debug -opt:0 
     if %errorlevel% neq 0 exit 1
-    Odin/odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -thread-count:1
+    Odin/odin.exe build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -thread-count:1
 ) else (
-    odin build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2
+    odin.exe build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2
 )
