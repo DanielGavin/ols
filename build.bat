f944af752802e8104a1892027e1d4c5fb03a469e
Daniel Gavin <danielgavin5@hotmail.com>
2021-09-21 00:07:33 +0200

    Change configs for ci build

--- build.bat
+++ build.bat
@@ -5,6 +5,8 @@ if "%1" == "CI" (
     set ODIN="Odin/odin"
     %ODIN% test tests -collection:shared=src -debug -opt:0 
     if %errorlevel% neq 0 if "%x1" == "CI" exit 1
+    %ODIN% build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -thread-count:1
+    exit 0
 ) else (
     set ODIN="odin"
 )
