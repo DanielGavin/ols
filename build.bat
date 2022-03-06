8e8293d21f83d20269309affbe79fa17c162ffb5
Daniel Gavin <danielgavin5@hotmail.com>
2022-03-06 14:45:13 +0100

    remove native on CI

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@
 if "%1" == "CI" (
     "Odin/odin.exe" test tests -collection:shared=src -debug -opt:0 
     if %errorlevel% neq 0 exit 1
-    "Odin/odin.exe" build src\ -show-timings -microarch:native -collection:shared=src -out:ols -opt:2 -thread-count:1
+    "Odin/odin.exe" build src\ -show-timings -collection:shared=src -out:ols -opt:2 -thread-count:1
 ) else if "%1" == "test" (
     odin test tests -collection:shared=src -debug -opt:0 
 ) else if "%1" == "single_test" (
