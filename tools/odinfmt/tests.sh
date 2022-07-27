43ad08345e30cc9c0994646c8037dde795b596d9
Daniel Gavin <danielgavin5@hotmail.com>
2022-07-27 16:21:55 +0200

    CI

--- tools/odinfmt/tests.sh
+++ tools/odinfmt/tests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 cd "${0%/*}"
+
 odin run tests.odin -file -show-timings  -collection:shared=../../src -out:tests.exe 
-if ([ $? -ne 0 ]) then exit 1 fi
+
+if ([ $? -ne 0 ]) 
+then 
+	exit 1 
+fi
