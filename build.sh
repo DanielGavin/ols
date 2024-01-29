8c0a8232071f1a1461795a040f6e2455e21b8a4d
Laytan Laats <laytanlaats@hotmail.com>
2024-01-29 23:01:21 +0100

    arm64 builds

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 
 if [[ $1 == "CI" ]]
 then
+	shift
+
     export PATH=$PATH:$PWD/Odin
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    odin test ../tests -collection:shared=../src -o:speed
+    odin test ../tests -collection:shared=../src -o:speed $@
 
     if ([ $? -ne 0 ])
     then
@@ -28,10 +30,14 @@ then
 fi
 if [[ $1 == "single_test" ]]
 then
+	shift
+
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    odin test ../tests -collection:shared=../src -test-name:$2
+    odin test ../tests -collection:shared=../src -test-name:$@
+
+	shift
 
     if ([ $? -ne 0 ])
     then
@@ -44,10 +50,12 @@ fi
 
 if [[ $1 == "test" ]]
 then
+	shift
+
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    odin test ../tests -collection:shared=../src
+    odin test ../tests -collection:shared=../src $@
 
     if ([ $? -ne 0 ])
     then
@@ -59,9 +67,11 @@ then
 fi
 if [[ $1 == "debug" ]]
 then
-    odin build src/ -collection:shared=src -out:ols -use-separate-modules -debug
+	shift
+
+    odin build src/ -collection:shared=src -out:ols -use-separate-modules -debug $@
     exit 0
 fi
 
 
-odin build src/ -collection:shared=src -out:ols -o:speed
+odin build src/ -collection:shared=src -out:ols -o:speed $@
