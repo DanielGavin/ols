5805fd0b688446eeb23528497972b9f934208f1a
DanielGavin <danielgavin5@hotmail.com>
2024-06-04 19:51:03 +0200

    Make tests single threaded

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 if "%1" == "CI" (
     set "PATH=%cd%\Odin;!PATH!"
 
-    rem odin test tests -collection:src=src 
+    rem odin test tests -collection:src=src -define:ODIN_TEST_THREADS=1
     rem if %errorlevel% neq 0 exit /b 1
     
     odin build src\ -collection:src=src -out:ols.exe -o:speed
