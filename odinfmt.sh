74ea70325e4daca321a0932828f236ea8c77f2ec
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2025-08-26 10:18:49 -0400

    Add option to disable document links

--- odinfmt.sh
+++ odinfmt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-odin build tools/odinfmt/main.odin -file -show-timings -collection:src=src -out:odinfmt -o:speed
+odin build tools/odinfmt/main.odin -file -show-timings -collection:src=src -out:odinfmt -o:none
