07e659b998b6a3754408a10bc6a400a99b8b79ca
thetarnav <gthetarnav@gmail.com>
2026-09-26 10:12:02 +0200

    Improve ci script
    - Make `ci.sh/bat` run build/test scripts using `build.sh/bat`
    - Add `./build.sh release` that drops the `-microarch:native` flag for
    github releases
    - Enables multi-threaded tests and memeory-tracking on CI (due to
    running ./build.sh test)

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-VERSION="dev-$(date -u '+%Y-%m-%d')-$(git rev-parse --short HEAD)"
-
 if [[ $1 == "single_test" ]]
 then
     shift
@@ -10,9 +7,7 @@ then
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    odin test ../tests -collection:src=../src -define:ODIN_TEST_NAMES=$@ -define:ODIN_TEST_FAIL_ON_BAD_MEMORY=true
-
-    shift
+    odin test ../tests -collection:src=../src -define:ODIN_TEST_NAMES="$@" -define:ODIN_TEST_FAIL_ON_BAD_MEMORY=true
 
     if ([ $? -ne 0 ])
     then
@@ -30,7 +25,7 @@ then
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    odin test ../tests -collection:src=../src $@ -define:ODIN_TEST_FAIL_ON_BAD_MEMORY=true
+    odin test ../tests -collection:src=../src "$@" -define:ODIN_TEST_FAIL_ON_BAD_MEMORY=true
 
     if ([ $? -ne 0 ])
     then
@@ -48,7 +43,7 @@ then
     #BUG in odin test, it makes the executable with the same name as a folder and gets confused.
     cd tests
 
-    odin build ../tests -build-mode:test -collection:src=../src $@ -define:ODIN_TEST_FAIL_ON_BAD_MEMORY=true
+    odin build ../tests -build-mode:test -collection:src=../src "$@" -define:ODIN_TEST_FAIL_ON_BAD_MEMORY=true
 
     if ([ $? -ne 0 ])
     then
@@ -59,12 +54,26 @@ then
 	exit 0
 fi
 
+if [[ -z "$OLS_VERSION" ]]; then
+	OLS_VERSION="dev-$(date -u '+%Y-%m-%d')-$(git rev-parse --short HEAD)"
+fi
+
+echo "OLS_VERSION=$OLS_VERSION"
+
+if [[ $1 == "release" ]]
+then
+    shift
+
+    odin build src/ -show-timings -collection:src=src -out:ols -no-bounds-check -o:speed -define:VERSION=$OLS_VERSION "$@"
+    exit 0
+fi
+
 if [[ $1 == "debug" ]]
 then
     shift
 
-    odin build src/ -show-timings -collection:src=src -out:ols -microarch:native -no-bounds-check -use-separate-modules -define:VERSION=$VERSION-debug -debug $@
+    odin build src/ -show-timings -collection:src=src -out:ols -microarch:native -no-bounds-check -use-separate-modules -define:VERSION=$OLS_VERSION-debug -debug "$@"
     exit 0
 fi
 
-odin build src/ -show-timings -collection:src=src -out:ols -microarch:native -no-bounds-check -o:speed -define:VERSION=$VERSION $@
+odin build src/ -show-timings -collection:src=src -out:ols -microarch:native -no-bounds-check -o:speed -define:VERSION=$OLS_VERSION "$@"
